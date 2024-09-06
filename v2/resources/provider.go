69ceabe205840be4222e26eef00f43550c6c18d7
Yury <xyuria@zededa.com>
2024-09-06 15:14:13 +0200

    changed

--- v2/resources/provider.go
+++ v2/resources/provider.go
@@ -18,6 +18,8 @@ import (
 
 var (
 	version   string = "dev"
+        defaultHost string = "zedcontrol.local.zededa.net"
+//        defaltToken string = ""
 )
 
 func Provider() *schema.Provider {
@@ -28,7 +30,7 @@ func Provider() *schema.Provider {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "ZEDCloud url. Ex: https://zedcontrol.zededa.net",
-				Default:     "zedcontrol.local.zededa.net",
+				Default:     defaultHost,
 				DefaultFunc: schema.EnvDefaultFunc("TF_VAR_zedcloud_url", nil),
 			},
 			"zedcloud_token": {
@@ -111,7 +113,10 @@ func (h *HttpTransportWrapper) RoundTrip(req *http.Request) (*http.Response, err
 
 func ProviderConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	zedCloudURL := d.Get("zedcloud_url").(string)
-        panic(os.Getenv("TF_VAR_zedcloud_url"))
+        if zedCloudURL == defaultHost {
+          zedCloudURL = os.Getenv("TF_VAR_zedcloud_url")
+          d.Set("zedcloud_url", zedCloudURL)
+        }
 	token, tokenIsSet := d.Get("zedcloud_token").(string)
 	if !tokenIsSet || token == "" {
 		return nil, diag.FromErr(errors.New("zedcloud API key must be set"))
